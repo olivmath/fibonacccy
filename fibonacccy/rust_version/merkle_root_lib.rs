fa7df467d8fcd89d59fe3473602aaf7fbb2dd7f4
Lucas Oliveira <olivmath@protonmail.com>
2023-11-05 01:55:22 -0300

    update: WIP merkle root

--- fibonacccy/rust_version/merkle_root_lib.rs
+++ fibonacccy/rust_version/merkle_root_lib.rs
@@ -1,8 +1,26 @@
 use std::slice;
 
 #[no_mangle]
-pub extern "C" fn modify_mutable_array(ptr: *mut u8, len: usize) {
+pub extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
     let slice = unsafe { slice::from_raw_parts_mut(ptr, len) };
     slice.reverse();
+}
 
+#[no_mangle]
+pub extern "C" fn reverse_immutable_array(ptr: *mut u8, len: usize) -> *mut u8 {
+    let array = unsafe { slice::from_raw_parts(ptr, len) };
+    let mut output_vec = array.to_vec();
+    output_vec.reverse();
+
+    let boxed_array = output_vec.into_boxed_slice();
+    let ptr = Box::into_raw(boxed_array) as *mut u8;
+
+    ptr
+}
+
+#[no_mangle]
+pub extern "C" fn free_immutable_array(ptr: *mut u8, len: usize) {
+    unsafe {
+        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len as usize));
+    }
 }
--- tests/test_merkle_root.py
+++ tests/test_merkle_root.py
@@ -1,15 +1,35 @@
 from ctypes import CDLL, c_ubyte, c_size_t, POINTER
 
 
-def test_modify_mutable_array():
+def test_reverse_mutable_array():
     lib = CDLL("fibonacccy/rust_version/libmerkle_root.dylib")
-    lib.modify_mutable_array.argtypes = (POINTER(c_ubyte), c_size_t)
-    lib.modify_mutable_array.restype = None
+    lib.reverse_mutable_array.argtypes = (POINTER(c_ubyte), c_size_t)
+    lib.reverse_mutable_array.restype = None
 
     data = b"lucas"
     array_type = c_ubyte * len(data)
     array_data = array_type(*data)
 
-    lib.modify_mutable_array(array_data, len(data))
+    lib.reverse_mutable_array(array_data, len(data))
 
     assert bytes(array_data) == b"sacul"
+
+
+def test_reverse_immutable_array():
+    lib = CDLL("fibonacccy/rust_version/libmerkle_root.dylib")
+    lib.reverse_immutable_array.argtypes = (POINTER(c_ubyte), c_size_t)
+    lib.reverse_immutable_array.restype = POINTER(c_ubyte)
+
+    lib.free_immutable_array.argtypes = (POINTER(c_ubyte), c_size_t)
+    lib.free_immutable_array.restype = None
+
+    data = b"lucas"
+    array_type = c_ubyte * len(data)
+    array_data = array_type(*data)
+
+    result_ptr = lib.reverse_immutable_array(array_data, len(data))
+    result = bytes(result_ptr[: len(data)])
+    lib.free_immutable_array(result_ptr, len(data))
+
+    assert result == b"sacul"
+
