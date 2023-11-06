3ef50c33c51d52c7cd992d41b9a8995b13c2986a
Lucas Oliveira <olivmath@protonmail.com>
2023-11-05 21:41:18 -0300

    update: WIP refactor merkle root pass list[bytes]

--- fibonacccy/rust_version/merkle_root_lib.rs
+++ fibonacccy/rust_version/merkle_root_lib.rs
@@ -1,5 +1,4 @@
 use std::slice;
-use std::mem;
 
 #[no_mangle]
 pub extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
@@ -25,64 +24,38 @@ pub extern "C" fn free_immutable_array(ptr: *mut u8, len: usize) {
         let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
     }
 }
-
 #[no_mangle]
 pub extern "C" fn reverse_immutable_array_of_arrays(
     array_of_arrays_ptr: *const *const u8,
     len: usize,
 ) -> *mut *mut u8 {
     let arrays = unsafe { slice::from_raw_parts(array_of_arrays_ptr, len) };
-    let mut result_ptrs = Vec::with_capacity(len);
 
-    for &inner_ptr in arrays {
-        let inner_array = unsafe { slice::from_raw_parts(inner_ptr, 5) };
-        let mut reversed_array = inner_array.to_vec();
-        reversed_array.reverse();
-        result_ptrs.push(reversed_array.into_boxed_slice());
-    }
+    let result_ptrs: Vec<_> = arrays
+        .iter()
+        .map(|&inner_ptr| {
+            let inner_array = unsafe { slice::from_raw_parts(inner_ptr, 5) };
+            inner_array
+                .iter()
+                .rev()
+                .cloned()
+                .collect::<Vec<u8>>()
+                .into_boxed_slice()
+        })
+        .map(|boxed_slice| Box::into_raw(boxed_slice) as *mut u8)
+        .collect();
 
-    let mut boxed_arrays: Vec<*mut u8> =
-        result_ptrs.iter_mut().map(|arr| arr.as_mut_ptr()).collect();
-    let ptr = boxed_arrays.as_mut_ptr();
-    mem::forget(result_ptrs);
-    mem::forget(boxed_arrays);
+    let ptr_array = Box::into_raw(result_ptrs.into_boxed_slice()) as *mut *mut u8;
 
-    ptr
+    ptr_array
 }
 
 #[no_mangle]
 pub extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
     unsafe {
-        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
+        let mut boxed_ptrs = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
+        boxed_ptrs.iter_mut().for_each(|&mut inner_ptr| {
+            let _ = Box::from_raw(inner_ptr);
+        });
     }
 }
-
-
-// #[no_mangle]
-// pub extern "C" fn reverse_immutable_array_of_arrays(
-//     array_of_arrays_ptr: *const *const u8,
-//     len: usize,
-// ) -> *mut *mut u8 {
-//     let arrays = unsafe { slice::from_raw_parts(array_of_arrays_ptr, len) };
-//     let mut result_ptrs = Vec::with_capacity(len);
-
-//     for &inner_ptr in arrays {
-//         let inner_array = unsafe { slice::from_raw_parts(inner_ptr, 5) };
-//         let mut reversed_array = inner_array.to_vec();
-//         reversed_array.reverse();
-//         let x = Box::into_raw(reversed_array.into_boxed_slice()) as *mut u8;
-//         result_ptrs.push(x);
-//     }
-
-//     let boxed_ptrs = result_ptrs.into_boxed_slice();
-//     let ptr_array = Box::into_raw(boxed_ptrs) as *mut *mut u8;
-
-//     ptr_array
-// }
-
-// #[no_mangle]
-// pub extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
-//     unsafe {
-//         let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
-//     }
-// }
