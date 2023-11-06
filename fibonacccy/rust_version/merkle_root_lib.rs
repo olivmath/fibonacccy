90dffebd2cbb0dff67a2c3de4280edc281b3a981
Lucas Oliveira <olivmath@protonmail.com>
2023-11-05 21:34:52 -0300

    update: WIP merkle root pass list[bytes]

--- fibonacccy/rust_version/merkle_root_lib.rs
+++ fibonacccy/rust_version/merkle_root_lib.rs
@@ -1,4 +1,5 @@
 use std::slice;
+use std::mem;
 
 #[no_mangle]
 pub extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
@@ -7,7 +8,7 @@ pub extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
 }
 
 #[no_mangle]
-pub extern "C" fn reverse_immutable_array(ptr: *mut u8, len: usize) -> *mut u8 {
+pub extern "C" fn reverse_immutable_array(ptr: *const u8, len: usize) -> *mut u8 {
     let array = unsafe { slice::from_raw_parts(ptr, len) };
     let mut output_vec = array.to_vec();
     output_vec.reverse();
@@ -21,6 +22,67 @@ pub extern "C" fn reverse_immutable_array(ptr: *mut u8, len: usize) -> *mut u8 {
 #[no_mangle]
 pub extern "C" fn free_immutable_array(ptr: *mut u8, len: usize) {
     unsafe {
-        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len as usize));
+        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
     }
 }
+
+#[no_mangle]
+pub extern "C" fn reverse_immutable_array_of_arrays(
+    array_of_arrays_ptr: *const *const u8,
+    len: usize,
+) -> *mut *mut u8 {
+    let arrays = unsafe { slice::from_raw_parts(array_of_arrays_ptr, len) };
+    let mut result_ptrs = Vec::with_capacity(len);
+
+    for &inner_ptr in arrays {
+        let inner_array = unsafe { slice::from_raw_parts(inner_ptr, 5) };
+        let mut reversed_array = inner_array.to_vec();
+        reversed_array.reverse();
+        result_ptrs.push(reversed_array.into_boxed_slice());
+    }
+
+    let mut boxed_arrays: Vec<*mut u8> =
+        result_ptrs.iter_mut().map(|arr| arr.as_mut_ptr()).collect();
+    let ptr = boxed_arrays.as_mut_ptr();
+    mem::forget(result_ptrs);
+    mem::forget(boxed_arrays);
+
+    ptr
+}
+
+#[no_mangle]
+pub extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
+    unsafe {
+        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
+    }
+}
+
+
+// #[no_mangle]
+// pub extern "C" fn reverse_immutable_array_of_arrays(
+//     array_of_arrays_ptr: *const *const u8,
+//     len: usize,
+// ) -> *mut *mut u8 {
+//     let arrays = unsafe { slice::from_raw_parts(array_of_arrays_ptr, len) };
+//     let mut result_ptrs = Vec::with_capacity(len);
+
+//     for &inner_ptr in arrays {
+//         let inner_array = unsafe { slice::from_raw_parts(inner_ptr, 5) };
+//         let mut reversed_array = inner_array.to_vec();
+//         reversed_array.reverse();
+//         let x = Box::into_raw(reversed_array.into_boxed_slice()) as *mut u8;
+//         result_ptrs.push(x);
+//     }
+
+//     let boxed_ptrs = result_ptrs.into_boxed_slice();
+//     let ptr_array = Box::into_raw(boxed_ptrs) as *mut *mut u8;
+
+//     ptr_array
+// }
+
+// #[no_mangle]
+// pub extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
+//     unsafe {
+//         let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
+//     }
+// }
--- tests/test_merkle_root.py
+++ tests/test_merkle_root.py
@@ -1,4 +1,5 @@
-from ctypes import CDLL, c_ubyte, c_size_t, POINTER
+from ctypes import CDLL, c_ubyte, c_size_t, POINTER, cast
+import pytest
 
 
 def test_reverse_mutable_array():
@@ -25,11 +26,42 @@ def test_reverse_immutable_array():
 
     data = b"lucas"
     array_type = c_ubyte * len(data)
-    array_data = array_type(*data)
+    array_ptr = array_type(*data)
 
-    result_ptr = lib.reverse_immutable_array(array_data, len(data))
+    result_ptr = lib.reverse_immutable_array(array_ptr, len(data))
     result = bytes(result_ptr[: len(data)])
     lib.free_immutable_array(result_ptr, len(data))
 
     assert result == b"sacul"
 
+
+def test_reverse_immutable_array_of_arrays():
+    lib = CDLL("fibonacccy/rust_version/libmerkle_root.dylib")
+    lib.reverse_immutable_array_of_arrays.argtypes = (
+        POINTER(POINTER(c_ubyte)),
+        c_size_t,
+    )
+    lib.reverse_immutable_array_of_arrays.restype = POINTER(POINTER(c_ubyte))
+
+    lib.free_array_of_arrays.argtypes = (POINTER(POINTER(c_ubyte)), c_size_t)
+    lib.free_array_of_arrays.restype = None
+
+    data = [b"lucas", b"grazy", b"elise"]
+
+    pointer_array_type = POINTER(c_ubyte) * len(data)
+    pointers = pointer_array_type(
+        *[cast((c_ubyte * 5).from_buffer_copy(d), POINTER(c_ubyte)) for d in data]
+    )
+
+    result_ptrs = lib.reverse_immutable_array_of_arrays(pointers, len(data))
+
+    result = [bytes(result_ptrs[i][:5]) for i in range(len(data))]
+    lib.free_array_of_arrays(result_ptrs, len(data))
+
+    assert len(result) == len(data)
+    for i in result:
+        assert len(i) == 5
+    assert result == [b"sacul", b"yzarg", b"esile"]
+    with pytest.raises(ValueError) as excinfo:
+        result = [bytes(result_ptrs[i][:5]) for i in range(len(data))]
+    assert "NULL pointer access" in str(excinfo.value)
