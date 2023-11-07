944151601921ac25c1ad981f59404e35a4f4899e
Lucas Oliveira <olivmath@protonmail.com>
2023-11-07 01:28:39 -0300

    update: WIP merkle root passing function

--- fibonacccy/rust_version/merkle_root_lib.rs
+++ fibonacccy/rust_version/merkle_root_lib.rs
@@ -1,31 +1,42 @@
 use std::slice;
 
+/// # Safety
+///
+/// essa funcao é um perigo menino
 #[no_mangle]
-pub extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
+pub unsafe extern "C" fn reverse_mutable_array(ptr: *mut u8, len: usize) {
     let slice = unsafe { slice::from_raw_parts_mut(ptr, len) };
     slice.reverse();
 }
 
+/// # Safety
+///
+/// essa funcao é um perigo menino
 #[no_mangle]
-pub extern "C" fn reverse_immutable_array(ptr: *const u8, len: usize) -> *mut u8 {
+pub unsafe extern "C" fn reverse_immutable_array(ptr: *const u8, len: usize) -> *mut u8 {
     let array = unsafe { slice::from_raw_parts(ptr, len) };
     let mut output_vec = array.to_vec();
     output_vec.reverse();
 
     let boxed_array = output_vec.into_boxed_slice();
-    let ptr = Box::into_raw(boxed_array) as *mut u8;
-
-    ptr
+    Box::into_raw(boxed_array) as *mut u8
 }
 
+/// # Safety
+///
+/// essa funcao é um perigo menino
 #[no_mangle]
-pub extern "C" fn free_immutable_array(ptr: *mut u8, len: usize) {
+pub unsafe extern "C" fn free_immutable_array(ptr: *mut u8, len: usize) {
     unsafe {
         let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
     }
 }
+
+/// # Safety
+///
+/// essa funcao é um perigo menino
 #[no_mangle]
-pub extern "C" fn reverse_immutable_array_of_arrays(
+pub unsafe extern "C" fn reverse_immutable_array_of_arrays(
     array_of_arrays_ptr: *const *const u8,
     len: usize,
 ) -> *mut *mut u8 {
@@ -45,17 +56,43 @@ pub extern "C" fn reverse_immutable_array_of_arrays(
         .map(|boxed_slice| Box::into_raw(boxed_slice) as *mut u8)
         .collect();
 
-    let ptr_array = Box::into_raw(result_ptrs.into_boxed_slice()) as *mut *mut u8;
-
-    ptr_array
+    Box::into_raw(result_ptrs.into_boxed_slice()) as *mut *mut u8
 }
 
+/// # Safety
+///
+/// essa funcao é um perigo menino
 #[no_mangle]
-pub extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
+pub unsafe extern "C" fn free_array_of_arrays(ptr: *mut *mut u8, len: usize) {
     unsafe {
         let mut boxed_ptrs = Box::from_raw(slice::from_raw_parts_mut(ptr, len));
         boxed_ptrs.iter_mut().for_each(|&mut inner_ptr| {
             let _ = Box::from_raw(inner_ptr);
         });
+    }
+}
+
+/// # Safety
+///
+/// essa funcao é um perigo menino
+#[no_mangle]
+pub unsafe extern "C" fn exec_hash(
+    callback: extern "C" fn(input: *const u8, output: *mut u8),
+    array_prt: *const u8,
+) -> *mut u8 {
+    let mut buffer: [u8; 32] = [0; 32];
+    callback(array_prt, buffer.as_mut_ptr());
+
+    let boxed_array = buffer.to_vec().into_boxed_slice();
+    Box::into_raw(boxed_array) as *mut u8
+}
+
+/// # Safety
+///
+/// essa funcao é um perigo menino
+#[no_mangle]
+pub unsafe extern "C" fn free_32(ptr: *mut u8) {
+    unsafe {
+        let _ = Box::from_raw(slice::from_raw_parts_mut(ptr, 32));
     }
 }
--- tests/test_merkle_root.py
+++ tests/test_merkle_root.py
@@ -1,4 +1,13 @@
-from ctypes import CDLL, c_ubyte, c_size_t, POINTER, cast
+from ctypes import (
+    CDLL,
+    CFUNCTYPE,
+    c_ubyte,
+    c_size_t,
+    POINTER,
+    cast,
+    memmove,
+)
+import hashlib
 import pytest
 
 
@@ -65,3 +74,35 @@ def test_reverse_immutable_array_of_arrays():
     with pytest.raises(ValueError) as excinfo:
         result = [bytes(result_ptrs[i][:5]) for i in range(len(data))]
     assert "NULL pointer access" in str(excinfo.value)
+
+
+def test_passing_function():
+    callback_function = CFUNCTYPE(None, POINTER(c_ubyte), POINTER(c_ubyte))
+
+    @callback_function
+    def python_callback(ptr, output_ptr):
+        data = bytes(ptr[:64])
+        hashed = hashlib.sha256(data)
+        memmove(output_ptr, hashed.digest(), 32)
+
+    lib = CDLL("fibonacccy/rust_version/libmerkle_root.dylib")
+    lib.exec_hash.argtypes = [callback_function, POINTER(c_ubyte)]
+    lib.exec_hash.restype = POINTER(c_ubyte)
+    lib.free_32.argtypes = [POINTER(c_ubyte)]
+    lib.free_32.restype = None
+
+    # Sample input data
+    array_data = b"3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb"
+    array_type = c_ubyte * 64
+    array_ptr = array_type(*array_data)
+
+    # Call Rust function which will call back into Python
+    result_ptr = lib.exec_hash(python_callback, array_ptr)
+
+    # Get the result from Rust
+    result = bytes(result_ptr[:32])
+    assert result.hex() == "df1055e0d9a89743ba6d0331cd81ecca579d5ffa250c109c4f2070cfdff9dbc4"
+    assert result == bytes.fromhex("df1055e0d9a89743ba6d0331cd81ecca579d5ffa250c109c4f2070cfdff9dbc4")
+
+    # When done with result, free the allocated memory from Rust side
+    lib.free_32(result_ptr)
